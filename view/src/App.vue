c85aa5a35c7a1896070e6e62d5be57edb6e8bff3
fanyushun <fanyushun@zj.tech>
2022-02-10 11:52:31 +0800

    feat: 添加view

---
+++ view/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { io } from "socket.io-client";
+
+const socket = io('http://localhost:3001');
+
+socket.on("connect", () => {
+  console.log(socket.id);
+  console.log(socket.connected);
+});
+
+socket.on("disconnect", () => {
+  console.log(socket.id);
+  console.log(socket.connected);
+});
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
